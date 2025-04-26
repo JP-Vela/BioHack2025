f298f82eb76b7baeae369a325408f940c85dff3e
JP-Vela <jangofett122003@gmail.com>
2025-04-26 14:49:47 -0700

    More models

--- inference.py
+++ inference.py
@@ -30,15 +30,26 @@ dataset_dir = 'dataset/our_data'
 # filename = '10sec.csv'
 # filename = 'subjectd-concentrating-2.csv'
 # filename = 'subjecta-neutral-1' # in original_data
-filename = 'JonnyUnderstanding'
 
+labels = {
+    "understanding": [0,0,1],
+    "confused": [1,0,0],
+    "control": [0,1,0],
+    "Understanding": [0,0,1],
+    "Confused": [1,0,0],
+    "Control": [0,1,0]
+}
+
+test_class = "Control"
+# filename = f'Jonny{test_class}'
+filename = f'JonnyConfused5'
 
 window_size = 256 #samples
 
 
 sub_df = pd.read_csv(f"{dataset_dir}/{filename}.csv") 
-cutoff = int(sub_df.shape[0]*0.85)
-sub_df = sub_df.iloc[cutoff:]
+cutoff = int(sub_df.shape[0]*0.8)
+# sub_df = sub_df.iloc[cutoff:]
 
 # NOTE: Split the data after reading, THEN perform sliding window
 
@@ -50,7 +61,7 @@ sample_count = 0
 row=0
 while row+window_size < sub_df.shape[0]:
     windows.append(sub_df.iloc[row:row+window_size])
-    row+=int(window_size*0.1)
+    row+=int(window_size*0.2)
 
 
 
@@ -62,13 +73,13 @@ windows = windows[:, :, 1:5]
 X_scalar = None
 
 # To load the data back
-with open('models/X_scalar.pckl', 'rb') as file:
+with open('models/X_scalar_jp.pckl', 'rb') as file:
     X_scalar = pickle.load(file)
 
 windows = transform_set(X_scalar, np.array(windows), fit=False)
 
 
-model = keras.models.load_model("models/3_output_model_jonny_2.keras", custom_objects=None, compile=True, safe_mode=True)
+model = keras.models.load_model("models/3_output_model_jps.keras", custom_objects=None, compile=True, safe_mode=True)
 preds = model.predict(windows)
 
 '''with open(f'results/{filename}.json', 'w') as f:
@@ -79,17 +90,25 @@ preds = model.predict(windows)
 score = 0
 
 # Confused, Control, Understanding
-for i in range(0, preds.shape[0], 3):
+# [1,0,0]   [0,1,0]  [0,0,1]
+for i in range(preds.shape[0]):
 
-    is_equal = (np.round(preds[i+1]) == np.array([0,0,1]))
-    
+    blank = np.zeros(3)
+    maxed = preds[i].argmax()
+    blank[maxed] = 1
+    is_equal = (np.round(preds[i]) == np.array(labels[test_class]))
+    # is_equal = (blank == np.array(labels[test_class]))
+
+    # confidence = preds[i].mean() - preds[i].min()
+    # print(blank, confidence)
+    print(preds[i])
     if is_equal.all():
         score+=1
         # print(preds[i])
         # plotit(X_test_scaled[i])
 
 
-for p in np.round(preds):
-    print(p)
+# for p in preds:#np.round(preds):
+    # print(p)
 
-print(f"{score}/{preds.shape[0]/3} = {score/(preds.shape[0]/3)*100}%")
+print(f"{score}/{preds.shape[0]} = {score/(preds.shape[0])*100}%")
